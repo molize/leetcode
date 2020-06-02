3b24fb93f777d2c2e2703b3d66c7ba393370bc6e
YCKJ2230 <lixiaohua@cloudwalk.cn>
2020-06-02 23:48:44 +0800

    @todo 20

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -14,19 +14,37 @@ var isValid = function(s) {
     var arr1 = ['(','{','[',];
     var arr2 = [')','}',']'];
 
-    if(s.length == 0 || s.length%2 != 0) {
+    if(s.length == 0) {
+        result = true;
+        return result
+    }
+    if(s.length%2 != 0) {
+        result = false;
         return result
     }
     var currStr = s.substr(0,1);
+    var lastStr = s.substr(s.length-1,1);
     
     if(!arr1.includes(currStr)) {
         return result
     }
 
-    var check = (start,end) => {
-        var startIndex = start || 0;
-        var endIndex = end || s.length-1;
-        if(startIndex < endIndex) {
+    var index = arr1.findIndex(str=> {
+        return str == currStr;
+    });
+    var index1 = arr2.findIndex(str=> {
+        return str == lastStr;
+    });
+
+    if(index == -1 || index1 == -1 ) {
+        return result
+    }
+
+    if(index == index1) {
+        var startIndex = 0;
+        var endIndex =  s.length-1;
+
+        while (startIndex < endIndex) {
             var _index = arr1.findIndex(str=> {
                 return str == s.substr(startIndex,1)
             });
@@ -36,19 +54,45 @@ var isValid = function(s) {
 
             if(_index == -1 || _index1 == -1 ) {
                 result = false;
-                return result
+                break;
             }
+            if(_index == _index1) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+            startIndex++;
+            endIndex--;
+        }
+    } else {
+        var checkStr = (start)=> {
+            var num1 = start || 0;
+            if(num1 < s.length-1) {
+                var _index = arr1.findIndex(str=> {
+                    return str == s.substr(num1,1)
+                });
+                var _index1 = arr2.findIndex(str=> {
+                    console.log(str,s.substr(num1+1,1))
+                    return str == s.substr(num1+1,1)
+                });
 
-            var fn1 = () => {
-                if(_index == _index1) {
-                    result = true;
-                    check(startIndex+1,endIndex-1)();
+                if(_index != -1 && _index1 != -1 ) {
+                    if(_index==_index1) {
+                        result = true;
+                        checkStr(num1+2);
+                    } else {
+                        result = false;
+                    }
                 } else {
                     result = false;
                 }
             }
         }
+        checkStr();
     }
+    return result
+    
 };
 // @lc code=end
 
